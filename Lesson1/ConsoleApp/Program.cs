0ee6d727f2e6120f9e8395e0b270f5d09c99945d
Anuri <ya-reutov@ya.ru>
2021-06-23 14:01:50 +0300

    [Lesson1] Console App reading name input

---
+++ Lesson1/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            Console.Write("Введите имя: ");
+            name += Console.ReadLine();
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine($"Привет, {name}, сегодня {dateTime}");
+            Console.ReadKey();
+        }
+    }
+}
