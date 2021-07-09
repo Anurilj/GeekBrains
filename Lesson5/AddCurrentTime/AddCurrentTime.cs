3055ccff5772a04f4644db6b4b90bd4b3c3b55a7
Anuri <ya-reutov@ya.ru>
2021-07-10 00:11:49 +0300

    [Lesson5] Task2 complete

---
+++ Lesson5/AddCurrentTime/AddCurrentTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace AddCurrentTime
+{
+    class AddCurrentTime
+    {
+        static void Main(string[] args)
+        {
+            File.AppendAllText("startup.txt", Environment.NewLine);
+            File.AppendAllText("startup.txt", DateTime.Now.ToString("HH:mm:ss"));
+        }
+    }
+}
