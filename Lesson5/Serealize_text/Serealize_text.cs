038edf9a1f7fb684e672e167a8925f750a27585e
Anuri <ya-reutov@ya.ru>
2021-07-09 21:56:36 +0300

    [Lesson5] Task 1 complete

---
+++ Lesson5/Serealize_text/Serealize_text.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+
+namespace Serealize_text
+{
+    class Serealize_text
+    {
+        static void Main(string[] args)
+        {
+            string filename = "output.txt";
+            
+            Console.Write("Введите Данные: ");
+            string outputDatata = Console.ReadLine();
+            File.WriteAllText(filename, outputDatata);
+        }
+    }
+}
