f481cc73317833a322edeeef103f0bd190b3075b
AnuriLJ <ya-reutov@ya.ru>
2021-07-23 00:45:13 +0300

    [Lesson8] Task complete

---
+++ Lesson8/DataVersionSave/DataVersionSave/DataVersionSave.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataVersionSave
+{
+    class DataVersionSave
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{Properties.Settings.Default.Greeting}");
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.Name))
+            {
+                Console.WriteLine("Введите имя:");
+                Properties.Settings.Default.Name = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            
+            if (string.IsNullOrEmpty(Properties.Settings.Default.Age))
+            {
+                Console.WriteLine("Введите возраст:");
+                Properties.Settings.Default.Age = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            
+            if (string.IsNullOrEmpty(Properties.Settings.Default.Job))
+            {
+                Console.WriteLine("Введите род деятельности:");
+                Properties.Settings.Default.Job = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+
+            Console.WriteLine($"Добрый день, {Properties.Settings.Default.Name}, возраст: {Properties.Settings.Default.Age}, род деятельности: {Properties.Settings.Default.Job}");
+        }
+    }
+}
